a508f22c359f756c85c7f8839b727c03800e20ba
Aabbaaii13 <photoman894@gmail.com>
2024-12-13 13:48:28 +0500

    Update ip-address.js

--- js/ip-address.js
+++ js/ip-address.js
@@ -1,29 +1,23 @@
-// Function to fetch the public IP addresses
-function getIPAddresses() {
-  // Fetch IPv4 address from ipify API
+function IPAddresses() {
   fetch('https://api.ipify.org?format=json')
     .then(response => response.json())
     .then(data => {
-      // Display the IPv4 address in the HTML
       document.getElementById('ipv4').textContent = data.ip;
     })
     .catch(error => {
-      console.error('Error fetching IPv4 address:', error);
-      document.getElementById('ipv4').textContent = 'Unable to fetch IPv4';
+      console.error('IPv4:', error);
+      document.getElementById('ipv4').textContent = 'You Do Not Have IPv4';
     });
 
-  // Fetch IPv6 address from ipify API
   fetch('https://api6.ipify.org?format=json')
     .then(response => response.json())
     .then(data => {
-      // Display the IPv6 address in the HTML
       document.getElementById('ipv6').textContent = data.ip;
     })
     .catch(error => {
-      console.error('Error fetching IPv6 address:', error);
-      document.getElementById('ipv6').textContent = 'Unable to fetch IPv6';
+      console.error('IPv6:', error);
+      document.getElementById('ipv6').textContent = 'You Do Not Have IPv6';
     });
 }
 
-// Call the function to display IP addresses when the page loads
-getIPAddresses();
+IPAddresses();
